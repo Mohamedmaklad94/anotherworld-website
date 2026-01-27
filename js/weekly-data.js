8d0dd493510bf0c2a934d5acbe2022b0fe2dc46a
Mohamed Maklad <mhamedatef33@gmail.com>
2026-01-27 06:01:10 +0200

    update header navbar to suit phones and add 2nd week photos

--- js/weekly-data.js
+++ js/weekly-data.js
@@ -49,17 +49,165 @@ const weeklyPhotos = [
     week: "الأسبوع الأول"
   },
   {
-    src: "img/weekphotos/01-08-2026-mera-photography.jpg",
-    author: "Mera Photography",
+    src: "img/weekphotos/01-09-2026-7abubacam.jpg",
+    author: "7abubacam",
     month: "يناير 2026",
-    week: "الأسبوع الأول"
+    week: "الأسبوع الثاني"
   },
+   {
+    src: "img/weekphotos/01-09-2026-asmaa-essam.jpg",
+    author: "أسماء عصام",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+   {
+    src: "img/weekphotos/01-09-2026-dina-alaa.jpg",
+    author: "Dina Alaa",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+   {
+    src: "img/weekphotos/01-09-2026-marwa-suleiman.jpg",
+    author: "Marwa Suleiman",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-10-2026-arwa-khalf.jpg",
+    author: "Arwa Khalf",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-09-2026-nada-m-harhara.jpg",
+    author: "Nada M Harhara",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-09-2026-walaa-ali.jpg",
+    author: "Walaa Ali",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-10-2026-ayman-shendy.jpg",
+    author: "Ayman Shendy",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-12-2026-mohammad-younis.jpg",
+    author: "Mohammad Younis",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-10-2026-galal-abdel-naser-el-shaer.jpg",
+    author: "Galal Abdel-naser El-Shaer",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-10-2026-hossam-abbas.jpg",
+    author: "Hossam Abbas",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-12-2026-kareem-farghaly.jpg",
+    author: "Kareem Farghaly",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-11-2026-walaa-ali.jpg",
+    author: "Walaa Ali",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
    {
     src: "img/weekphotos/01-08-2026-omar-saad.jpg",
     author: "Omar Saad",
     month: "يناير 2026",
-    week: "الأسبوع الأول"
+    week: "الأسبوع الثاني"
   },
 
+   {
+    src: "img/weekphotos/01-12-2026-norhan.jpg",
+    author: "نورهان",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-13-2026-gehad-abdelfattah.jpg",
+    author: "جهاد عبدالفتاح",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-15-2026-ahmed-mohamed.jpg",
+    author: "Ahmed Mohamed",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-15-2026-saeed-ahmed.jpg",
+    author: " Saeed Ahmed",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-15-2026-norhan-mohammed.jpg",
+    author: "Norhan Mohammed",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+
+   {
+    src: "img/weekphotos/01-15-2026-ahmed-rasull.jpg",
+    author: "Ahmed Rasull",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+    {
+    src: "img/weekphotos/01-10-2026-mohamed-abozaid.jpg",
+    author: "Mohamed Abozaid",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+    {
+    src: "img/weekphotos/01-10-2026-mahmoud-abu-seedo.jpg",
+    author: "Mahmoud Abu Seedo",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+    {
+    src: "img/weekphotos/01-10-2026-manar-elmkawy.jpg",
+    author: "Manar Elmkawy",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+    {
+    src: "img/weekphotos/01-10-2026-mohammed-shaban.jpg",
+    author: "Mohammed Shaban",
+    month: "يناير 2026",
+    week: "الأسبوع الثاني"
+  },
+  
 
 ];
